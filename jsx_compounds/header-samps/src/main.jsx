649f257e208911e90188e89ae1e802d45654df2b
Felipe <fesantacruz99@gmail.com>
2024-06-05 12:57:39 -0300

    Initial commit

---
+++ jsx_compounds/header-samps/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Header from './Header.jsx'
+import './styles/Globals.css'
+ 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+)
